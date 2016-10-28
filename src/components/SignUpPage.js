f82c41719d5bb6650fe8430441a248665fdad25c
colinwhat <ctaylor@whatcounts.com>
2016-10-28 16:59:19 -0400

    update

--- src/components/SignUpPage.js
+++ src/components/SignUpPage.js
@@ -1,10 +1,15 @@
 import React , { Component } from 'react'
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
-
+// var firebase = require('firebase/app');
+// all 3 are optional and you only need to require them at the start
+import firebase from 'firebase/app';
+require('firebase/auth');
 
 class AboutPage extends Component {
     render() {
+        // var provider = new firebase.auth.GoogleAuthProvider();
+
         return (
             <div className="content">
                 <TextField
