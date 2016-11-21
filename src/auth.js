39032e61ec89bd51388ec247e68cdb416dd346f5
Colin <colinrileytaylor@gmail.com>
2016-11-20 20:17:23 -0500

    tweaked protected routes

--- src/auth.js
+++ src/auth.js
@@ -1,17 +1,7 @@
 import * as firebase from 'firebase';
 import config from './firebase.config.js';
 firebase.initializeApp(config);
-// var loggedInUser;
-// firebase.auth().onAuthStateChanged(function(user) {
-//   if (user) {
-//     // User is signed in.
-//     loggedInUser = user;
-//     console.log(user)
-//   } else {
-//     // No user is signed in.
-//     user = null;
-//   }
-// });
+
 function requireAuth(nextState, replace) {
     if (!firebase.auth().currentUser) {
         let hasLocalStorageUser = false;
@@ -29,15 +19,6 @@ function requireAuth(nextState, replace) {
         }
     }
 };
-// function requireAuth(nextState, replace) {
-// console.log('firebase user ' +firebase.auth().currentUser)
-//     if(typeof loggedInUser === 'undefined') {
-//       console.log('was undefined')
-//         replace({
-//           pathname: '/',
-//           state: { nextPathname: nextState.location.pathname }
-//         })
-//     }
-// }
+
 
 export default requireAuth;
