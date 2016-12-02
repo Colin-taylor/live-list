002936b2ca98e65d7afc4752540055322678d4cb
Colin <colinrileytaylor@gmail.com>
2016-12-01 20:53:20 -0500

    changed checkbox component

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react'
 
-// import Divider from 'material-ui/Divider';
+import {Checkbox, List, ListItem, IconButton }from 'material-ui/';
 import Delete from 'material-ui/svg-icons/action/delete';
-import BlankCheck from 'material-ui/svg-icons/toggle/check-box-outline-blank';
-import Checkbox from 'material-ui/svg-icons/toggle/check-box';
+// import BlankCheck from 'material-ui/svg-icons/toggle/check-box-outline-blank';
+// import Checkbox from 'material-ui/svg-icons/toggle/check-box';
 import MobileTearSheet from './MobileTearSheet';
 import autobind from 'react-autobind';
 
+const DeleteButton = (props) => (
+    <IconButton onClick={props.onDeleteClick}>
+        <Delete/>
+    </IconButton>
+);
 
 class ShoppingList extends Component {
     constructor(props) {
@@ -25,24 +30,20 @@ class ShoppingList extends Component {
     render() {
         return (
             <MobileTearSheet>
-                <ul className="list">
-                {this.props.items.map((item, index) => (
-                        <li key={index} className="row between-xs">
-                        {item.completed ? 
-                            <Checkbox onClick={() => this.onCheckClick(item)}/> 
-                        : 
-                        <BlankCheck
-                        onClick={() => this.onCheckClick(item)}
-                        />
-                        }
-                            <span className={this.isCompleted(item)}>{item.name}</span>
-                            <Delete
-                            className="delete-icon" 
-                            onClick={() => this.onDeleteClick(item)}/>
-                        
-                        </li>
-                ))}
-                </ul>
+                <List>
+                {this.props.items.map((item, index) => {
+                    return (
+                    <ListItem
+                    key={index}
+                    leftCheckbox={<Checkbox />}
+                    primaryText={item.name}
+                    rightIconButton={<IconButton onClick={()=> this.onDeleteClick(item)}>
+                                        <Delete/>
+                                    </IconButton>}
+                />
+                    
+                )})}
+                </List>
             </MobileTearSheet>
         );
     }
