187957bd59500981dd1d9d618a142fd831525626
Colin <colinrileytaylor@gmail.com>
2017-01-09 20:23:23 -0500

    tweaked sw settings again

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -3,11 +3,12 @@ module.exports = {
   staticFileGlobs: [
     'build/*.html',
     'build/manifest.json',
-    'build/static/**/!(*map*)'
+    'build/static/**/!(*map*)',
+    '//cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css'
   ],
   dontCacheBustUrlsMatching: /\.\w{8}\./,
   swFilePath: 'build/service-worker.js',
   navigateFallback: '/index.html',
-  navigateFallbackWhitelist: [/^\/www.googleapis.com\//]
-
+  //navigateFallbackWhitelist: [/^\/handler\//]
+  navigateFallbackWhitelist: [/^(?!\/__)/, /^\/handler\//]
 };
